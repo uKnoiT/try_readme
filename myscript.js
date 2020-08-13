2660627b0aaba82836453447dbc74d10890ae1f3
uKnoiT <arihantjain065@gmail.com>
2020-08-14 03:26:56 +0530

    adding new update to both html & js file for hosting

--- myscript.js
+++ myscript.js
@@ -1,19 +1,16 @@
-// const btnGetLatestRepos = document.getElementById('btnGetLatestRepos');
-// btnGetLatestRepos.addEventListener('mouseover', get5Repos);
+    // const btnGetLatestRepos = document.getElementById('btnGetLatestRepos');
+    // btnGetLatestRepos.addEventListener('mouseover', get5Repos);
 
 window.addEventListener('load',get5Repos);
 
-const divResult = document.getElementById('divResult');
+const ul1 = document.getElementById('ul1');
 
 async function get5Repos() {
-    // clear();
+// clear();
     
     const url = "https://api.github.com/users/cod-lab/repos?sort=created";     // get my repos in desc order they created
     const response = await fetch(url);
     const result = await response.json();
-
-    // console.log(result)
-    // result.forEach(i=> {
 
     var j=-1;
     for(var i in result) {
@@ -22,26 +19,45 @@ async function get5Repos() {
             var li = document.createElement('li');
             var a = li.appendChild(document.createElement('a'));
 
-            // const anchor = document.createElement('a')
+                // const anchor = document.createElement('a')
+
+                // const str1 = "-";
+                // const str2 = str1.fontcolor('black');
+                // const str1 = '  - ';
+                // const str2 = result[i].full_name.replace('cod-lab/','&nbsp;');
             a.href = result[i].html_url;
-            a.textContent = result[i].full_name.replace('cod-lab/','');
-            // a.textContent.fontcolor('red');
+                // str2 = str1;
+                // a.textContent = str1.concat(str2);
+                // const str3 = ' - ';
+                // const str4 = str1.fontcolor('red').concat(str2);
+                // a.textContent = str4;
 
-            // anchor.classList.add("cls1")
+            a.textContent = result[i].full_name.replace('cod-lab/',' - ');
+                // a = a.concat(' ',)
+                // a.textContent.fontcolor('red');
 
-            // divResult.appendChild(anchor)
-            // divResult.appendChild(document.createElement('br'))
-            divResult.appendChild(li);
+                // anchor.classList.add("cls1")
+
+                // divResult.appendChild(anchor)
+                // divResult.appendChild(document.createElement('br'))
+            ul1.appendChild(li);
 
             ++j;
         }
         if(j==4) break;
     }
+
+    html2canvas(document.getElementById('sec'))
+        .then(function (canvas) {
+            var imgdata = canvas.toDataURL("image/png",0.9);
+
+            document.getElementById('image').src=imgdata;
+    
+            console.log(imgdata);
+        })
 }
 
 // async function clear() {              // clear earlier result before printing new
 //     while(divResult.firstChild)
 //         divResult.removeChild(divResult.firstChild);
 // }
-
-console.log('this is js file');
